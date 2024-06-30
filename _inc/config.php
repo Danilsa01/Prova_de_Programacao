d97de0250e51f09c4ee0a8e4ed4ca0fe294e229e
Danilsa Ginga <103261752+Danilsa01@users.noreply.github.com>
2024-06-30 22:55:38 +0100

    Add files via upload

---
+++ _inc/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('API_VERSION',           'v1.0.0');
+
+// api enabled or disabled
+define('API_ACTIVE',            true);
+define('API_MESSAGE',           '');
+
+// mysql config
+define('MYSQL_HOST',            'localhost');
+define('MYSQL_DATABASE',        'systemtransport');
+define('MYSQL_USER',            'ishasillan');
+define('MYSQL_PASS',            'angola1234');
